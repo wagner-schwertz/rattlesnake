9913ab866b1a3823985d099f350f4fb03808d4d1
Wagner Schwertz dos Santos <wagner.santos@ntconsult.com.br>
2020-09-14 03:10:07 -0300

    Updates

---
+++ precache-manifest.911e1672e83b435abc1e085c4248bbae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922e70f9c8236164033420c65badf432",
+    "url": "/rattlesnake/index.html"
+  },
+  {
+    "revision": "0943c9c5c79e0bf0b5f2",
+    "url": "/rattlesnake/static/js/2.bafea0fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rattlesnake/static/js/2.bafea0fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef21a08bc99f7b8b655a",
+    "url": "/rattlesnake/static/js/main.2f127547.chunk.js"
+  },
+  {
+    "revision": "d53cf995d7adf902837f",
+    "url": "/rattlesnake/static/js/runtime-main.6e307b56.js"
+  }
+]);
