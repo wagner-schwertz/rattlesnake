eff55ed4574b7569b5bfa013615fadd88a35408d
Wagner Schwertz dos Santos <wagner.santos@ntconsult.com.br>
2020-09-14 19:20:58 -0300

    Updates

---
+++ precache-manifest.ad427b9bae2ba36692007629076f3f0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85634b2572799890c5f83bddf5c8c326",
+    "url": "/rattlesnake/index.html"
+  },
+  {
+    "revision": "0943c9c5c79e0bf0b5f2",
+    "url": "/rattlesnake/static/js/2.bafea0fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rattlesnake/static/js/2.bafea0fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41231ea7df441aecb379",
+    "url": "/rattlesnake/static/js/main.a7852964.chunk.js"
+  },
+  {
+    "revision": "d53cf995d7adf902837f",
+    "url": "/rattlesnake/static/js/runtime-main.6e307b56.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rattlesnake/precache-manifest.9a7e6f7fe1fed8d44db0c80d566c7ed7.js"
+  "/rattlesnake/precache-manifest.ad427b9bae2ba36692007629076f3f0d.js"
 );
 
 self.addEventListener('message', (event) => {
